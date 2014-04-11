59d3a29cfc04750cc6ccf307599c9f42c3f08b93
mattroy <mattroy@ccs.neu.edu>
2014-04-11 02:04:05 -0400

    add sounds and load in main ui.

--- Sythen/client/javascript/sythen.js
+++ Sythen/client/javascript/sythen.js
@@ -1,5 +1,94 @@
 //sythen.js
 
+var queue = [];
+var newQueue = [];
+var baseTime;
+var playInterval;
+var playing = false;
+
+function loadSounds() {
+    
+    var audioPath = "sounds/";
+    var manifest = [
+        {id: "1", src: "sounds/binary_drop_01.ogg"},
+        {id: "2", src: "sounds/binary_drop_02.ogg"},
+        {id: "3", src: "sounds/binary_drop_03.ogg"},
+        {id: "4", src: "sounds/binary_drop_04.ogg"},
+        {id: "5", src: "sounds/binary_drop_05.ogg"},
+        {id: "6", src: "sounds/binary_drop_06.ogg"},
+        {id: "7", src: "sounds/binary_drop_07.ogg"},
+        {id: "8", src: "sounds/binary_drop_08.ogg"},
+        {id: "9", src: "sounds/binary_drop_09.ogg"},
+        {id: "10", src: "sounds/binary_drop_10.ogg"},
+        {id: "11", src: "sounds/binary_drop_11.ogg"},
+        {id: "12", src: "sounds/binary_drop_12.ogg"}
+    ];
+
+    createjs.Sound.addEventListener("loadComplete", handleSoundLoad);
+    createjs.Sound.registerManifest(manifest, audioPath);
+
+}
+
+function setupSynthButtons() {
+	$("#synth > .synthButton").click(handleSoundButton);
+    //$("#startButton").click(handleStart);
+    //$("#stopButton").click(handleStop);
+    //$("#playButton").click(handlePlay);
+}
+
+function handleSoundLoad(event) {
+    console.log("Audio " + event.src + " was loaded.");
+}
+
+function handleSoundButton(event) {
+    createjs.Sound.play(event.target.getAttribute("data-sound-id"));
+    console.log("time " + (Date.now() - baseTime) + " event " + event.target.innerText);
+    if (playing) {
+        newQueue.push({time: (Date.now() - baseTime), id: event.target.innerText});
+    } else {
+        queue.push({time: (Date.now() - baseTime), id: event.target.innerText});
+    }
+}
+
+function handleStart() {
+    baseTime = Date.now();
+    
+    console.log("Start time is " + baseTime);
+}
+
+function handleStop() {
+    console.log("stop");
+    clearInterval(playInterval);
+    playing = false;
+    queue = queue.concat(newQueue);
+    queue = queue.sort(function(a, b) {
+        a.time < b.time ? a : b;
+    });
+    newQueue = [];
+}
+
+function handlePlay() {
+    var time = 0;
+    var pos = 0;
+    playing = true;
+
+    baseTime = Date.now();
+
+    playInterval = setInterval(function() {
+        console.log("Time is " + time + " and pos is " + pos);
+
+        if (pos >= queue.length) {
+            clearInterval(playInterval);
+        }
+
+        while (pos < queue.length && queue[pos].time < time) {
+            createjs.Sound.play(queue[pos].id);
+            pos++;
+        }
+        time += 50;
+    }, 50);
+}
+
 $(function() {
 	console.log("initializing...");
 	
@@ -16,6 +105,9 @@ $(function() {
 	/*Setup Player*/
 	
 	/*Load Sounds*/
+	loadSounds();
+	setupSynthButtons();
 	
 	//TODO: remove web dependencies
 });
+
